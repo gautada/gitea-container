94c54c3c0e03cf60ecad14059fec99a71f69ac0a
Adam T. Gautier <adam@gautier.org>
2022-06-05 22:35:19 -0400

    Attempting a podman/drone build

--- 10-ep-container.sh
+++ 10-ep-container.sh
@@ -8,15 +8,8 @@ TEST="$(/usr/bin/pgrep gitea)"
 if [ $? -eq 1 ] ; then
  echo "---------- [ GIT SERVER(gitea) ] ----------"
  if [ -z "$ENTRYPOINT_PARAMS" ] ; then
-  # /usr/bin/gitea --config /opt/gitea/app.ini --work-path /opt/gitea --custom-path /opt/gitea/customv web--port 8080
   /usr/bin/gitea --config /opt/gitea/app.ini --work-path /opt/gitea --custom-path /opt/gitea/custom web
  fi
- 
-#
-#   /usr/bin/sudo /usr/sbin/nginx -g "daemon off;"
-#  else
-#   /usr/bin/sudo /usr/sbin/nginx
-#  fi
 fi
 
 
